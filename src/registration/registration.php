fe0edb4b68c353d6bd1356b497f86a57a24fb174
Edinorig <super-ddred@ukr.net>
2022-10-29 19:19:37 +0200

    temp

--- src/registration/registration.php
+++ src/registration/registration.php
@@ -1,12 +1,44 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style-registration.css">
     <title>Registration</title>
 </head>
+
 <body>
-    
+    <header>
+        <section>
+
+        <div class="nav-bar"><p>Home</p></div>
+
+        </section>
+    </header>
+    <main>
+        <section>
+            <div class="wrapper-form-registration">
+                <form action="." method="post">
+                    <p class="form-name">Registration</p>
+                    <input type="text" name="input-login" class="inputText form-input">
+                    <input type="text" name="input-psw" class="inputNumber form-input">
+                    <button type="submit">
+                        Submit
+                    </button>
+                </form>
+            </div>
+            <div class="separate-line">
+
+            </div>
+            <div class="wrapper-link-registration">
+
+            </div>
+        </section>
+    </main>
+
 </body>
+<script type="module" src="./js/view-registration.js"></script>
+
 </html>
