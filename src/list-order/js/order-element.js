6ddc536fc879d62c4975a49cc7dd355066afe4e1
Artur Smirnov <artur.smirnov@innovatrieste.it>
2022-11-02 20:28:17 +0100

    update html / setters

--- src/list-order/js/order-element.js
+++ src/list-order/js/order-element.js
@@ -5,7 +5,7 @@ class OrderElement {
         this.productId = null;
 
         const parser = new DOMParser();
-        const templateString = `<div class="wrapper-order">    <div class="horizontal-line">    </div>    <div class="wrapper-block">        <div class="wrapper-date">            <h4></h4>        </div>        <div class="wrapper-block-list">            <h4>Your order:</h4>            <ul>                <li>                    <h5 class="wrapper-product"></h5>                </li>                <li>                    <h5 class="wrapper-product"></h5>                </li>            </ul>        </div>        <div class="wrapper-address-price-block">            <div class="wrapper-address">                <h4>Address :</h4>            </div>            <div class="wrapper-price">                <h4>Price:<span class="fuchsia"></span></h4>            </div>        </div>    </div></div>`;
+        const templateString = `<div class="wrapper-order"><div class="horizontal-line"></div><div class="wrapper-block"><div class="wrapper-date"><h4></h4></div><div class="wrapper-block-list"><h4>Your order:</h4><ul class="wrapper-product"></ul></div><div class="wrapper-address-price-block"><div class="wrapper-address"><h4></h4></div><div class="wrapper-price"><h4>Price:<span class="fuchsia" id="total-price"></span></h4></div></div></div></div>`;
         const templateElement = parser.parseFromString(templateString, 'text/html');
         this.template = templateElement.documentElement.querySelector("body > div");
     }
@@ -35,7 +35,14 @@ class OrderElement {
 
 
     setOrderProduct(product) {
-        this.elements.orderProduct.innerHTML = product.toString();
+        const templateProduct = `
+        <li>
+            <h5 class="wrapper-product">
+            ${product}
+            </h5>
+        </li>`;
+
+        this.elements.orderProduct.innerHTML += templateProduct;
     }
 
     setOrderPrice(price) {
