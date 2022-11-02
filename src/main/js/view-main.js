a2d528f9457ccc90ba20add846486ea47053247a
Artur Smirnov <artur.smirnov@innovatrieste.it>
2022-11-02 16:51:12 +0100

    (#9) desc section & product grid

--- src/main/js/view-main.js
+++ src/main/js/view-main.js
@@ -1,6 +1,6 @@
 import ProductElement from './product-element.js';
 
-let temp = new ProductElement(document.querySelector('.temp'));
+let temp = new ProductElement(document.querySelector('.wrapper-products'));
 temp.init();
 temp.setProductId('23');
 temp.setProductImg('../img/');
@@ -8,10 +8,18 @@ temp.setProductTag('Pz');
 temp.setProductName('Pane');
 temp.setProductPrice('4.55$');
 
-let temp2 = new ProductElement(document.querySelector('.temp'));
+let temp2 = new ProductElement(document.querySelector('.wrapper-products'));
 temp2.init();
 temp2.setProductId('24');
 temp2.setProductImg('../img/');
 temp2.setProductTag('Pa');
 temp2.setProductName('Pane Super');
 temp2.setProductPrice('5.55$');
+
+let temp3 = new ProductElement(document.querySelector('.wrapper-products'));
+temp3.init();
+temp3.setProductId('24');
+temp3.setProductImg('../img/');
+temp3.setProductTag('Pa');
+temp3.setProductName('Pane Super');
+temp3.setProductPrice('5.55$');
--- src/main/main.php
+++ src/main/main.php
@@ -15,15 +15,38 @@
 <body>
     <section>
         <header>
-           <object data="">
-            
-           </object>
+            <object data="">
+
+            </object>
         </header>
     </section>
     <main>
         <section>
-            <div class="temp">
+            <div class="wrapper-dersc-main">
+                <div class="wrapper-text">
+                    <h4>
+                        Take a look of our products
+                    </h4>
+                    <h6>
+                        gh an of up attempt gravity. Situation to be at offending elsewhere distrusts if. Particular use for considered projection cultivated. Worth of do doubt shall it their. Extensive existence up me contained he pronounce do. Excellence inquietude assistance precaution any impression man sufficient.
+                    </h6>
+                </div>
+                <div class="wrapper-img">
 
+                </div>
+            </div>
+            <div class="wrapper-week-products">
+                <div class="wrapper-title">
+                    <h3>
+                        Take a look of our week products
+                    </h3>
+                    <div class="wrapper-filtrs">
+
+                    </div>
+                </div>
+                <div class="wrapper-products">
+
+                </div>
             </div>
         </section>
     </main>
