4b034cff4130d5beb08e2f0639225a0f931c559d
Edinorig <super-ddred@ukr.net>
2022-11-02 17:32:51 +0100

    (#13)Update account page

--- src/account/account.php
+++ src/account/account.php
@@ -40,7 +40,7 @@
     </section>
     <main class="wrapper-main-block">
         <section>
-            <div class="wrapper-form" id="modify-credentianl" style="display: block;">
+            <div class="wrapper-form" id="modify-credentianl" >
                 <form method="post" name="modify-credentianl">
                     <input type="email" name="input-email" placeholder="Email" maxlength="30" autocomplete="current-login">
                     <input type="password" name="input-psw" placeholder="Password" minlength="12" autocomplete="current-password">
@@ -56,13 +56,13 @@
         <section>
             <div class="wrapper-form" id="modify-personal-data">
                 <form method="post" name="modify-personal-data">
-                    <input type="text" name="input-name" placeholder="Nome" maxlength="30">
-                    <input type="text" name="input-surname" placeholder="Cognome" maxlength="30">
+                    <input type="text" name="input-name" placeholder="Name" maxlength="30" class="inputOnlyText">
+                    <input type="text" name="input-surname" placeholder="Surname" maxlength="30" class="inputOnlyText">
 
-                    <input type="tel" name="input-tel" placeholder="Telefono" maxlength="12" class="marginTop">
-                    <input type="text" name="input-street" placeholder="Indirizzo" maxlength="40">
-                    <input type="text" name="input-cap" placeholder="CAP" maxlength="5">
-                    <input type="text" name="input-city" placeholder="Città" maxlength="20">
+                    <input type="text" name="input-phone" placeholder="Phone" maxlength="12" class="marginTop">
+                    <input type="text" name="input-street" placeholder="Street" maxlength="40">
+                    <input type="text" name="input-zip" placeholder="ZIP" maxlength="5" class="inputOnlyNumber">
+                    <input type="text" name="input-city" placeholder="City" maxlength="20" class="inputOnlyText">
                     <button type="submit" class="large">
                         <h4 class="light">
                             Apply
--- src/account/js/account-validation-personal-data.js
+++ src/account/js/account-validation-personal-data.js
@@ -1,6 +1,6 @@
 import UtilsForm from '../../common/js/utlis-form.js';
 
-class ValidationFormCredentionals
+class ValidationFormPersonalData
 {
     constructor(parentElement) {
         this.rootElement = parentElement;
@@ -18,16 +18,18 @@ class ValidationFormCredentionals
         this.utilsForm.init();
 
         this.elements = {
-            formRegistration: this.rootElement.querySelector('form[name="modify-credentianl"]'),
-            inputEmail: this.rootElement.querySelector('input[name="input-email"]'),
-            inputPsw: this.rootElement.querySelector('input[name="input-psw"]'),
-            inputPswHash: this.rootElement.querySelector('input[name="input-psw-hash"]'),
+            formRegistration: this.rootElement.querySelector('form[name="modify-personal-data"]'),
+            inputName: this.rootElement.querySelector('input[name="input-name"]'),
+            inputSurname: this.rootElement.querySelector('input[name="input-surname"]'),
+            inputTel: this.rootElement.querySelector('input[name="input-phone"]'),
+            inputStreet: this.rootElement.querySelector('input[name="input-street"]'),
+            inputCAP: this.rootElement.querySelector('input[name="input-zip"]'),
+            inputCity: this.rootElement.querySelector('input[name="input-city"]'),
         };
     }
 
     initEventListener() {
         this.elements.formRegistration.addEventListener('submit', (event) => {
-            /* debugger */
             const emptyElements = this.utilsForm.getEmptyInput();
 
             console.log("Hello world");
@@ -44,4 +46,5 @@ class ValidationFormCredentionals
         });
     }
 }
-export default ValidationFormCredentionals;
+
+export default ValidationFormPersonalData;
