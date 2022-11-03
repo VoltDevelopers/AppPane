5d660695624caef7d245f709495d54bdd723947f
Edinorig <super-ddred@ukr.net>
2022-11-03 20:30:41 +0100

    (#15) Upadte bugs

--- src/list-order/list-order.php
+++ src/list-order/list-order.php
@@ -63,34 +63,10 @@
         <section>
             <h3 class="wrapper-name">Your orders:</h3>
             <div class="wrapper-order">
-    <div class="horizontal-line">
 
     </div>
     <div class="wrapper-block">
-        <div class="wrapper-date">
-            <h4>Your order: 14 Settembre 2022</h4>
-        </div>
-
-        <div class="wrapper-block-list">
-            <h4>Your order:</h4>
-            <ul>
-                <li>
-                    <h5 class="wrapper-product">Mouse</h5>
-                </li>
-                <li>
-                    <h5 class="wrapper-product">Mouse</h5>
-                </li>
-            </ul>
-        </div>
-
-        <div class="wrapper-address-price-block">
-            <div class="wrapper-address">
-                <h4>Adress: Via del ronco 18</h4>
-            </div>
-            <div class="wrapper-price">
-                <h4>Price:<h4 class="price">25.46$</h4></h4>
-            </div>
-        </div>
+        
     </div>
 </div>
         </section>
