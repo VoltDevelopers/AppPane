01a4ad9b7885e2a9bee0bdc6a2128400288db9ad
enry04 <enrico04v@gmail.com>
2022-11-03 11:56:37 +0100

    (#11) addition of remove article, modify quantity

--- src/bag/js/product-in-bag-element.js
+++ src/bag/js/product-in-bag-element.js
@@ -4,9 +4,10 @@ class ProductInBagElement {
     constructor(parentElement) {
         this.rootElement = parentElement;
         this.elements = {};
+        this.nProductsInCart = 5;
         
         const parser = new DOMParser();
-        const templateString = '<div class="article-wrapper"><div class="image-wrapper"><div class="product-type-wrapper"></div></div><div class="right-wrapper"><div class="product-header-wrapper"><h3 class="product-name">pane pizza</h3><button type="button" class="remove-article-btn">X</button></div><div class="description-wrapper"><h6 class="product-description">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper</h6></div><div class="product-footer-wrapper"><div class="add-remove-quantity-wrapper"><button type="button" class="add-quantity-btn">+</button><h4 class="current-quantity">0</h4><button type="button" class="remove-quantity-btn">-</button></div><div class="price-wrapper"><h4 class="product-price">Total: <span class = "fuchsia">43.53$</span></h4></div></div></div></div>';
+        const templateString = '<div class="article-wrapper"><div class="image-wrapper"><div class="product-type-wrapper"></div></div><div class="right-wrapper"><div class="product-header-wrapper"><h3 class="product-name">pane pizza</h3><button type="button" class="remove-article-btn">X</button></div><div class="description-wrapper"><h6 class="product-description">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper</h6></div><div class="product-footer-wrapper"><div class="add-remove-quantity-wrapper"><button type="button" class="add-quantity-btn">+</button><h4 class="current-quantity">1</h4><button type="button" class="remove-quantity-btn">-</button></div><div class="price-wrapper"><h4 class="product-price">Total: <span class = "fuchsia">43.53$</span></h4></div></div></div></div>';
         const templateElement = parser.parseFromString(templateString, 'text/html');
         this.template = templateElement.documentElement.querySelector("body > div");
     }
@@ -18,44 +19,69 @@ class ProductInBagElement {
 
     initElement() {
 
-        this.elements = {
-            productName: this.template.querySelector(".product-name"),
-            btnRemoveArticle: this.template.querySelector(".remove-article-btn"),
-            productDescription: this.template.querySelector(".product-description"),
-            btnAddQuantity: this.template.querySelector(".add-quantity-btn"),
-            currentQuantity: this.template.querySelector(".current-quantity"),
-            btnRemoveQuantity: this.template.querySelector(".remove-quantity-btn"),
-            productPrice: this.template.querySelector(".product-price"),
-        }
-
-
-
-       //this.rootElement.appendChild(this.template);
-       let nProductsInCart = 5;
-       for (let i = 0; i < nProductsInCart; i++){
+       for (let i = 0; i < this.nProductsInCart; i++){
             let item = this.template.cloneNode(true);
             item.setAttribute("id", i);
             this.rootElement.appendChild(item);
         }
 
-        console.log(this.rootElement);
+        this.elements = {
+            productName: this.rootElement.querySelectorAll(".product-name"),
+            btnRemoveArticle: this.rootElement.querySelectorAll(".remove-article-btn"),
+            productDescription: this.rootElement.querySelectorAll(".product-description"),
+            btnAddQuantity: this.rootElement.querySelectorAll(".add-quantity-btn"),
+            currentQuantity: this.rootElement.querySelectorAll(".current-quantity"),
+            btnRemoveQuantity: this.rootElement.querySelectorAll(".remove-quantity-btn"),
+            productPrice: this.rootElement.querySelectorAll(".product-price"),
+        }
+
+        for(let i = 0; i < this.nProductsInCart;i++){
+            this.elements.productName[i].setAttribute("id", i);
+            this.elements.btnRemoveArticle[i].setAttribute("id", i);
+            this.elements.productDescription[i].setAttribute("id", i);
+            this.elements.btnAddQuantity[i].setAttribute("id", i);
+            this.elements.currentQuantity[i].setAttribute("id", i);
+            this.elements.btnRemoveQuantity[i].setAttribute("id", i);
+            this.elements.productPrice[i].setAttribute("id", i);
+        }
+        console.log(this.elements);
     }
 
     initEventListener() {
-        this.elements.btnRemoveArticle.addEventListener('click', this.onBtnRemoveArticleClick);
-        this.elements.btnAddQuantity.addEventListener('click', this.onBtnAddQuantityClick);
-        this.elements.btnRemoveQuantity.addEventListener('click', this.onBtnRemoveQuantityClick);
+        for (let i = 0; i < this.nProductsInCart; i++) {
+            this.onBtnRemoveArticleClick = this.onBtnRemoveArticleClick.bind(this);
+            this.elements.btnRemoveArticle[i].addEventListener('click', this.onBtnRemoveArticleClick);
+            this.onBtnAddQuantityClick = this.onBtnAddQuantityClick.bind(this);
+            this.elements.btnAddQuantity[i].addEventListener('click', this.onBtnAddQuantityClick);
+            this.onBtnRemoveQuantityClick = this.onBtnRemoveQuantityClick.bind(this);
+            this.elements.btnRemoveQuantity[i].addEventListener('click', this.onBtnRemoveQuantityClick);
+        }
+        
+    }
+    onBtnRemoveArticleClick(e) { 
+        let itemToRemove = document.getElementById(e.target.id);
+        itemToRemove.remove();
+        this.nProductsInCart--;
+        this.rootElement.innerHTML = '';
+        this.init();
     }
 
-    onBtnRemoveArticleClick() {
-        console.log("jc");
+    onBtnAddQuantityClick(e){
+        let item = document.getElementById(e.target.id);
+        let currentQuantity = item.querySelector(".current-quantity");
+        let newQuantity = parseInt(currentQuantity.textContent);
+        newQuantity++;
+        currentQuantity.innerText = newQuantity;
     }
 
-    onBtnAddQuantityClick(){
-
-    }
-
-    onBtnRemoveQuantityClick(){
+    onBtnRemoveQuantityClick(e){
+        let item = document.getElementById(e.target.id);
+        let currentQuantity = item.querySelector(".current-quantity");
+        let newQuantity = parseInt(currentQuantity.textContent);
+        if(newQuantity > 1){
+            newQuantity--;
+            currentQuantity.innerText = newQuantity;
+        }
 
     }
 
