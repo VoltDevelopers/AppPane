c43056ef319e212787d0190cf563e0d721d5a178
enry04 <enrico04v@gmail.com>
2022-11-04 17:59:44 +0100

    (#11) implementing cart functions

--- src/bag/js/product-in-bag-element.js
+++ src/bag/js/product-in-bag-element.js
@@ -6,8 +6,9 @@ class ProductInBagElement {
         this.elements = {};
         // this.nProductsInCart = 5;
         
+        this.productId = null;
         const parser = new DOMParser();
-        const templateString = '<div class="article-wrapper"><div class="image-wrapper"><div class="product-type-wrapper"></div></div><div class="right-wrapper"><div class="product-header-wrapper"><h4 class="product-name">pane pizza</h4><button type="button" class="remove-article-btn">X</button></div><div class="description-wrapper"><h6 class="product-description">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper</h6></div><div class="product-footer-wrapper"><div class="add-remove-quantity-wrapper"><button type="button" class="add-quantity-btn">+</button><h4 class="current-quantity">1</h4><button type="button" class="remove-quantity-btn">-</button></div><div class="price-wrapper"><h5 class="product-price">Total: <span class = "fuchsia">43.53$</span></h5></div></div></div></div>';
+        const templateString = '<div class="article-wrapper"><div class="image-wrapper"><div class="product-type-wrapper"></div></div><div class="right-wrapper"><div class="product-header-wrapper"><h4 class="product-name"></h4><button type="button" class="remove-article-btn">X</button></div><div class="description-wrapper"><h6 class="product-description"></h6></div><div class="product-footer-wrapper"><div class="add-remove-quantity-wrapper"><button type="button" class="add-quantity-btn">+</button><h4 class="current-quantity"></h4><button type="button" class="remove-quantity-btn">-</button></div><div class="price-wrapper"><h5 class="product-price">Total: <span class = "fuchsia"></span></h5></div></div></div></div>';
         const templateElement = parser.parseFromString(templateString, 'text/html');
         this.template = templateElement.documentElement.querySelector("body > div");
     }
@@ -26,14 +27,17 @@ class ProductInBagElement {
     //     }
 
         this.elements = {
-            productName: this.rootElement.querySelector(".product-name"),
-            btnRemoveArticle: this.rootElement.querySelector(".remove-article-btn"),
-            productDescription: this.rootElement.querySelector(".product-description"),
-            btnAddQuantity: this.rootElement.querySelector(".add-quantity-btn"),
-            currentQuantity: this.rootElement.querySelector(".current-quantity"),
-            btnRemoveQuantity: this.rootElement.querySelector(".remove-quantity-btn"),
-            productPrice: this.rootElement.querySelector(".product-price"),
+            wrapperProduct: this.template,
+            productName: this.template.querySelector(".product-name"),
+            btnRemoveArticle: this.template.querySelector(".remove-article-btn"),
+            productDescription: this.template.querySelector(".product-description"),
+            btnAddQuantity: this.template.querySelector(".add-quantity-btn"),
+            currentQuantity: this.template.querySelector(".current-quantity"),
+            btnRemoveQuantity: this.template.querySelector(".remove-quantity-btn"),
+            productPrice: this.template.querySelector(".product-price"),
         }
+
+        this.rootElement.appendChild(this.template);
 
         // for(let i = 0; i < this.nProductsInCart;i++){
         //     this.elements.productName[i].setAttribute("id", i);
@@ -48,43 +52,51 @@ class ProductInBagElement {
     }
 
     initEventListener() {
-        // for (let i = 0; i < this.nProductsInCart; i++) {
-        //     this.onBtnRemoveArticleClick = this.onBtnRemoveArticleClick.bind(this);
-        //     this.elements.btnRemoveArticle[i].addEventListener('click', this.onBtnRemoveArticleClick);
-        //     this.onBtnAddQuantityClick = this.onBtnAddQuantityClick.bind(this);
-        //     this.elements.btnAddQuantity[i].addEventListener('click', this.onBtnAddQuantityClick);
-        //     this.onBtnRemoveQuantityClick = this.onBtnRemoveQuantityClick.bind(this);
-        //     this.elements.btnRemoveQuantity[i].addEventListener('click', this.onBtnRemoveQuantityClick);
-        // }
+        this.onBtnRemoveArticleClick = this.onBtnRemoveArticleClick.bind(this);
+        this.elements.btnRemoveArticle.addEventListener('click', this.onBtnRemoveArticleClick);
+        this.onBtnAddQuantityClick = this.onBtnAddQuantityClick.bind(this);
+        this.elements.btnAddQuantity.addEventListener('click', this.onBtnAddQuantityClick);
+        this.onBtnRemoveQuantityClick = this.onBtnRemoveQuantityClick.bind(this);
+        this.elements.btnRemoveQuantity.addEventListener('click', this.onBtnRemoveQuantityClick);
         
     }
     onBtnRemoveArticleClick(e) { 
-        // let itemToRemove = document.getElementById(e.target.id);
-        // itemToRemove.remove();
-        // this.nProductsInCart--;
-        // this.rootElement.innerHTML = '';
-        // this.init();
+        this.elements.wrapperProduct.remove();
     }
 
-    onBtnAddQuantityClick(e){
-        // let item = document.getElementById(e.target.id);
-        // let currentQuantity = item.querySelector(".current-quantity");
-        // let newQuantity = parseInt(currentQuantity.textContent);
-        // newQuantity++;
-        // currentQuantity.innerText = newQuantity;
+    onBtnAddQuantityClick(){
+        let newQuantity = parseInt(this.elements.currentQuantity.textContent) + 1;
+        this.elements.currentQuantity.innerHTML = newQuantity;
     }
 
-    onBtnRemoveQuantityClick(e){
-        // let item = document.getElementById(e.target.id);
-        // let currentQuantity = item.querySelector(".current-quantity");
-        // let newQuantity = parseInt(currentQuantity.textContent);
-        // if(newQuantity > 1){
-        //     newQuantity--;
-        //     currentQuantity.innerText = newQuantity;
-        // }
-
+    onBtnRemoveQuantityClick(){
+        let newQuantity = parseInt(this.elements.currentQuantity.textContent);
+        if(newQuantity > 1){
+             newQuantity--;
+             this.elements.currentQuantity.innerHTML = newQuantity;
+        }
     }
 
+    setProductInBagId(id){
+        this.id = id;
+    }
+
+    setProductInBagName(name){
+        this.elements.productName.innerHTML = name;
+    }
+
+    setProductInBagDescription(description){
+        this.elements.productDescription.innerHTML = description;
+    }
+
+    setProductInBagCurrentQuantity(currentQuantity){
+        this.elements.currentQuantity.innerHTML = currentQuantity;
+    }
+
+    setProductInBagPrice(price){
+        let text = this.elements.productPrice.textContent + price;
+        this.elements.productPrice.innerHTML = text;
+    }
 }
 
 export default ProductInBagElement;
