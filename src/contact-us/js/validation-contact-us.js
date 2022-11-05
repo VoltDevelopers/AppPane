5631694ff234c92c3e9d9137188efd6250a98621
Edinorig <super-ddred@ukr.net>
2022-11-05 21:39:44 +0100

    (#56) Set contact us to data base

--- src/contact-us/js/validation-contact-us.js
+++ src/contact-us/js/validation-contact-us.js
@@ -1,4 +1,6 @@
 import UtilsForm from '../../common/js/utlis-form.js';
+import CookieManager from '../../common/js/cookie-manager.js';
+import UtilsFetch from '../../common/js/utils-fetch.js';
 
 class ValidationContactUS {
     constructor(parentElement) {
@@ -6,6 +8,7 @@ class ValidationContactUS {
         this.elements = {};
 
         this.utilsForm = new UtilsForm(this.rootElement);
+        this.utilsFetch = new UtilsFetch();
     }
 
     init() {
@@ -29,16 +32,39 @@ class ValidationContactUS {
             event.preventDefault();
 
             const emptyElements = this.utilsForm.getEmptyInput();
+            const userAuth = CookieManager.getCookie('user_auth');
 
             if (emptyElements == null) {
-            }
-            else {
+                if (userAuth) {
+                    const data = {
+                        'email': this.elements.inputEmail.value,
+                        'name': this.elements.inputUser.value,
+                        'notes': this.elements.inputProblem.value,
+                        'user': userAuth,
+                    };
+
+                    this.utilsFetch.postData('./php/contact-us.php', data)
+                        .then(response => {
+                            if (response.status == '200') {
+                                location.href = '../main/main.php';
+                            } else {
+                                console.log(response.data);
+                            }
+                        });
+                } else {
+                    location.href = "../login/login.php";
+                }
+            } else {
                 emptyElements.forEach(element => {
                     element.style.border = "2px solid red";
                 });
             }
+
+
+
+
         });
-}
+    }
 }
 
 export default ValidationContactUS;
