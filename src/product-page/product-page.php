f652e05774c84986c20c8aeddb442a684ffb6acf
Artur Smirnov <artur.smirnov@innovatrieste.it>
2022-11-05 21:04:44 +0100

    update header

--- src/product-page/product-page.php
+++ src/product-page/product-page.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,55 +9,19 @@
     <link rel="stylesheet" href="../common/css/style-page.css">
     <link rel="stylesheet" href="../common/css/style-font.css">
     <link rel="stylesheet" href="../common/css/style-popup.css">
-    <link rel="shortcut icon" href="../common/img/icon-shopping-bag.svg"/>
-    <link rel="stylesheet" href="./css/product-page-style.css"/>
+    <link rel="shortcut icon" href="../common/img/icon-shopping-bag.svg" />
+    <link rel="stylesheet" href="./css/product-page-style.css" />
 </head>
+
 <body>
-<section>
-    <header>
-            <div class="wrapper-header-left wrapper-header">
-                <h5>
-                    <a class="active-menu" href="../main/main.php">Home</a>
-                </h5>
-                <h5>
-                    <a href="#">Contacts</a>
-                </h5>
-            </div>
-            <div class="wrapper-header-center wrapper-header ">
-                <div class="icon-logo">
-                </div>
-            </div>
-            <div class="wrapper-header-right wrapper-header ">
-                <h5>
-                    <a href="../list-order/list-order.php">Orders</a>
-                </h5>
-                <h5 class="wrapper-popup-block">
-                    <a href="../account/account.php">Account</a>
-                    <div class="wrapper-popup-menu">
-                        <div class="popup-arrow ">
-
-                        </div>
-                        <div class="popup-inner">
-                            <div class="popup-line">
-                                <div class="wrapper-popup-icon-gear">
-
-                                </div>
-                                <a href="../account/account.php">Settings</a>
-                            </div>
-                            <div class="popup-line">
-                                <div class="wrapper-popup-icon-arrow">
-
-                                </div>
-                                <a href="../main/main.php">Logout</a>
-                            </div>
-                        </div>
-                    </div>
-                </h5>
-                <a href="../bag/bag.php">
-                    <div class="icon-shopping-bag"></div>
-                </a>
-
-            </div>
+    <section>
+        <header>
+            <?php
+            require('../common/php/token-manager.php');
+            $page = 'main';
+            $active = 'class="active-menu"';
+            require('../common/php/header.php');
+            ?>
         </header>
     </section>
     <section>
@@ -67,4 +32,5 @@
 
 </body>
 <script type="module" src="./js/view-product-page.js"></script>
+
 </html>
