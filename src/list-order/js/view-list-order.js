d5cb198048781fa0353efd2143f246dbe99a8d9c
enrico visentin <enrico04v@gmail.com>
2022-11-08 22:42:25 +0100

    (#88) init generation of orders

--- src/list-order/js/view-list-order.js
+++ src/list-order/js/view-list-order.js
@@ -1,6 +1,8 @@
 import OrderElement from './order-element.js';
+import CookieManager from "../../common/js/cookie-manager.js";
+import UtilsFetch from "../../common/js/utils-fetch.js";
 
-let temp = new OrderElement(document.querySelector('.wrapper-order'));
+/*let temp = new OrderElement(document.querySelector('.wrapper-order'));
 temp.init();
 temp.setOrderId('23');
 temp.setOrderDate('22 Settembre 2022');
@@ -19,4 +21,28 @@ temp2.setOrderProduct('Choccolate bread');
 temp2.setOrderProduct('Choccolate bread');
 temp2.setOrderProduct('Choccolate bread');
 temp2.setOrderDate('22 Novembre 2022');
-temp2.setOrderAddress('Via del pane 27');
+temp2.setOrderAddress('Via del pane 27');*/
+
+const clientId = 22;
+
+const data = {
+
+    'orderId': clientId,
+
+};
+
+UtilsFetch.postData('./php/product-connection.php', data)
+    .then((response) => {
+        if(response.status == '200'){
+            console.log(response.data);
+
+        }else {
+
+            console.log(response.data);
+
+        }
+
+
+    });
+
+
