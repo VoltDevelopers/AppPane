111c6dab5a899a2d8dfd59ebad18ebdfeed1e405
enry04 <enrico04v@gmail.com>
2022-11-08 08:07:55 +0100

    update

--- src/product-page/php/product-connection.php
+++ src/product-page/php/product-connection.php
@@ -4,10 +4,6 @@ require('../../common/php/connection.php');
 $connMySQL = new ConnectionMySQL();
 $pdo = $connMySQL->getConnection();
 
-print_r($pdo);
-
-/*
-
 $json = file_get_contents('php://input');
 $data = json_decode($json);
 
@@ -15,7 +11,6 @@ $idProduct = $data->idProduct;
 
 $stmt = $pdo->prepare('SELECT * FROM tprodotti WHERE id = $idProduct');
 $stmt->execute(['nome' => $productName, 'descrizione' => $productDescription, 'prezzo' => $productPrice, 'foto' => $productImage]);
-echo json_encode($productName);
 $result = null;
 $product = $stmt->fetch();
 
@@ -31,15 +26,6 @@ if($product != null){
     );
 
 }
-echo json_encode($result);
-*/
-
-$result = array(
-
-    'productName' => "ciaso",
-
-);
-
 echo json_encode($result);
 
 ?>
