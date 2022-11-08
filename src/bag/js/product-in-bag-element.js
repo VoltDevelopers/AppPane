6c576d77bf889010a268bb428571492339d4d298
enry04 <enrico04v@gmail.com>
2022-11-08 09:44:58 +0100

    (#82)

--- src/bag/js/product-in-bag-element.js
+++ src/bag/js/product-in-bag-element.js
@@ -8,7 +8,7 @@ class ProductInBagElement {
         
         this.productId = null;
         const parser = new DOMParser();
-        const templateString = '<div class="article-wrapper"><div class="image-wrapper"><div class="product-type-wrapper"></div></div><div class="right-wrapper"><div class="product-header-wrapper"><h4 class="product-name"></h4><button type="button" class="remove-article-btn">X</button></div><div class="description-wrapper"><h6 class="product-description"></h6></div><div class="product-footer-wrapper"><div class="add-remove-quantity-wrapper"><button type="button" class="add-quantity-btn">+</button><h4 class="current-quantity"></h4><button type="button" class="remove-quantity-btn">-</button></div><div class="price-wrapper"><h5 class="product-price">Total: <span class = "fuchsia"></span></h5></div></div></div></div>';
+        const templateString = '<div class="article-wrapper"><div class="image-wrapper"><div class="product-type-wrapper"></div></div><div class="right-wrapper"><div class="product-header-wrapper"><h4 class="product-name">pane pizza</h4><div class="icon-close"></div></div><div class="description-wrapper"><h6 class="product-description">Pellentesque habitant morbi tristique senectus et netus et malesuada famesac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Doneceu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.Quisque sit amet est et sapien ullamcorper</h6></div><div class="product-footer-wrapper"><div class="add-remove-quantity-wrapper"><div class="icon-plus"></div><h4 class="current-quantity">1</h4><div class="icon-minus"></div></div><div class="price-wrapper"><h5 class="product-price">Total: <span class="fuchsia">43.53$</span></h5></div></div></div></div>';
         const templateElement = parser.parseFromString(templateString, 'text/html');
         this.template = templateElement.documentElement.querySelector("body > div");
     }
@@ -22,7 +22,7 @@ class ProductInBagElement {
         this.elements = {
             wrapperProduct: this.template,
             productName: this.template.querySelector(".product-name"),
-            btnRemoveArticle: this.template.querySelector(".remove-article-btn"),
+            btnRemoveArticle: this.template.querySelector(".icon-close"),
             productDescription: this.template.querySelector(".product-description"),
             btnAddQuantity: this.template.querySelector(".add-quantity-btn"),
             currentQuantity: this.template.querySelector(".current-quantity"),
@@ -42,7 +42,7 @@ class ProductInBagElement {
         this.elements.btnRemoveQuantity.addEventListener('click', this.onBtnRemoveQuantityClick);
         
     }
-    onBtnRemoveArticleClick(e) { 
+    onBtnRemoveArticleClick() { 
         this.elements.wrapperProduct.remove();
     }
 
