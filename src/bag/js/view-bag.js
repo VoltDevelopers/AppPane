27d0a10cb931b2c33d9022ced0034f663f58f63e
Artur Smirnov <artur.smirnov@innovatrieste.it>
2022-11-08 13:30:25 +0100

    order rep

--- src/bag/js/view-bag.js
+++ src/bag/js/view-bag.js
@@ -1,15 +1,21 @@
 import CookieManager from "../../common/js/cookie-manager.js";
 import UtilsFetch from '../../common/js/utils-fetch.js';
 import ProductInBagElement from './product-in-bag-element.js';
+import ManagerCreateOrder from "./manager-create-order.js";
 
 const wrapperProducts = document.querySelector('.articles-wrapper');
+const wrapperOrder = document.querySelector('.order-all');
 const data = {
     'idClient': CookieManager.getCookie('user_auth'),
     'token': CookieManager.getCookie('user_id'),
 };
 
+const temp = new ManagerCreateOrder(wrapperOrder);
+temp.init();
+
 const productInBagList = [];
 const productList = [];
+let totalPrice = 0;
 
 await UtilsFetch.postData('./php/bag-product.php', data)
     .then(response => {
@@ -24,11 +30,15 @@ await UtilsFetch.postData('./php/bag-product.php', data)
                 product.setProductInBagDescription(productElement['descrizione']);
                 product.setProductInBagCurrentQuantity(productElement['quantita']);
                 product.setProductInBagPrice(productElement['prezzo']);
-
                 productList.push(product);
+
+                totalPrice += parseInt(productElement['prezzo']);
+                temp.setOrderProduct(productElement['nome'], productElement['quantita']);
             });
+            temp.setTotalPrice(totalPrice);
         } else {
             console.log(response);
             // todo log error
         }
     });
+
