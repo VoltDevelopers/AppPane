39ecd777069c2586dd42e8aa08a5431b33cb83bf
Edinorig <super-ddred@ukr.net>
2022-11-08 13:50:28 +0100

    (#83) added line in bag and modify structure of html

--- src/bag/bag.php
+++ src/bag/bag.php
@@ -5,11 +5,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>shopping bag page</title>
+    <title>Shopping bag page</title>
     <link rel="stylesheet" href="../common/css/style-page.css">
     <link rel="stylesheet" href="../common/css/style-font.css">
     <link rel="stylesheet" href="css/style-shopping-bag.css">
     <link rel="stylesheet" href="../common/css/style-popup.css">
+    <link rel="stylesheet" href="../list-order/css/style-list-order.css">
+    <link rel="shortcut icon" href="../common/img/icon-shopping-bag.svg" />
 </head>
 
 <body>
@@ -24,15 +26,21 @@
         </header>
     </section>
     <main>
-        <h3 style="text-align: center; margin-bottom: 30px;">Your shopping bag</h3>
-        <div class="articles-wrapper">
+        <section>
 
-        </div>
-        <div class="order-all">
-            <button>
-                Order All
-            </button>
-        </div>
+            <h3 class="wrapper-name">Your shopping bag</h3>
+            <div class="wrapper-order">
+
+                <div class="wrapper-block">
+                    <div class="horizontal-line"> </div>
+                    <div class="order-all">
+                        <button>
+                            Order All
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </section>
     </main>
     <?php include '../common/php/footer.php'; ?>
 </body>
