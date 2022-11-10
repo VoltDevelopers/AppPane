e3d85158315a5ed3d24677c0ae8e7fd92847c412
Edinorig <super-ddred@ukr.net>
2022-11-10 02:32:58 +0100

    #(118) Added alert when purshare product

--- src/main/js/product-element.js
+++ src/main/js/product-element.js
@@ -43,14 +43,14 @@ class ProductElement {
 
             if (data.idClient) {
                 UtilsFetch.postData('../common/php/add-product-to-bag.php', data)
-                .then(response => {
-                    if (response.status == '200') {
-                        // todo alert
-                        console.log('Added');
-                    } else {
-                        console.log(response.data);
-                    }
-                });
+                    .then(response => {
+                        if (response.status == '200') {
+                            // todo alert
+                            console.log('Added');
+                        } else {
+                            console.log(response.data);
+                        }
+                    });
             } else {
                 if (!CookieManager.getCookie('temp_bag_product_index')) {
                     CookieManager.setCookie('temp_bag_product_index', '0', 60 * 60);
@@ -59,13 +59,13 @@ class ProductElement {
                 CookieManager.setCookie(`temp_product_in_bag_${index}`, JSON.stringify(data), 60 * 60);
                 CookieManager.setCookie('temp_bag_product_index', index, 60 * 60);
             }
-            AlertExtend.showAlert("aggiunto al carrello", "aggiunta al carrello effettuata con successo");   
+            AlertExtend.showAlert("aggiunto al carrello", "aggiunta al carrello effettuata con successo");
 
         });
         this.elements.productImg.addEventListener('click', (event) => {
             CookieManager.setCookie('temp_id_product', this.productId, 180);
             location.href = '../product-page/product-page.php';
-        });     
+        });
     }
 
     showProduct() {
